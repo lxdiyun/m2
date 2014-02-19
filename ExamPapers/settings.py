bfb2099163552bbaefd97915b7c25b8e7b390891
Manik Jain <manik92@gmail.com>
2014-02-19 21:59:02 +0800

    yolo!

--- ExamPapers/settings.py
+++ ExamPapers/settings.py
@@ -145,6 +145,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 	'ExamPapers.DBManagement',
+    'ExamPapers.templatetags',
 	'resource',
     'haystack',
 	'dajaxice',
